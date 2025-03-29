d3cc8297a2b8f187861468d823c0f9cd43d94b43
MacdonaldRobinson <MacdonaldRobinson@users.noreply.github.com>
2025-03-29 23:17:53 +0000

    deploy: 61de61546efe1c027f0ac5ed679fb4d2aac97047

---
+++ assets/HomePage-D6gawXKv.js
@@ -0,0 +1,50 @@
+import{d as e,R as i,j as t,r as l,B as c}from"./index-BOG_CWlT.js";import{W as d}from"./WrapWithBgImage-M9q9tV72.js";const m=e.section`
+  height: 90vh;
+`,p=e.div`
+  height: 100%;
+  overflow: auto;
+  scrollbar-width: thin;
+  scroll-behavior: smooth;
+  scroll-snap-type: y mandatory;
+`,n=e.h3`
+  font-size: 2rem;
+`,a=e.div``,g=e.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  color: white;
+  gap: 20px;
+  padding: 20px;
+  scroll-snap-align: center;
+
+  @keyframes slideInDown {
+    from {
+      opacity: 0;
+      transform: translateY(-100%);
+    }
+    to {
+      opacity: 1;
+      transform: translateY(0%);
+    }
+  }
+
+  @keyframes slideOutUp {
+    from {
+      opacity: 1;
+      transform: translateY(0%);
+    }
+    to {
+      opacity: 0;
+      transform: translateY(-100%);
+    }
+  }
+
+  ${n}, ${a} {
+    opacity: 0;
+    animation: slideInDown 1s 0.5s forwards;
+  }
+`,h=i.memo(({blogPost:o,bgImageUrl:s})=>t.jsx(d,{bgImageUrl:s,children:t.jsxs(g,{children:[t.jsx(n,{children:o.title}),t.jsx(a,{children:o.body})]})})),y=()=>{const s=l.useContext(c).blogPosts;return t.jsx(m,{children:t.jsx(p,{children:s.map(r=>t.jsx(h,{blogPost:r,bgImageUrl:`https://via.assets.so/game.png?id=${r.id}&q=95&w=360&h=360&fit=fill`},r.id))})})};export{y as default};
---
+++ assets/WrapWithBgImage-M9q9tV72.js
@@ -0,0 +1,24 @@
+import{d as i,r as l,j as e}from"./index-BOG_CWlT.js";const n=i.img`
+  z-index: -1;
+`,g=i.div`
+  background-color: rgba(0, 0, 0, 0.5);
+  backdrop-filter: blur(5px);
+`,d=i.div`
+  width: 100%;
+  height: 100%;
+  position: relative;
+  opacity: 0;
+  transition: all 1s;
+
+  &.show {
+    opacity: 1;
+  }
+
+  ${n}, ${g} {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
+`;function p(t){var o,r,a="";if(typeof t=="string"||typeof t=="number")a+=t;else if(typeof t=="object")if(Array.isArray(t)){var s=t.length;for(o=0;o<s;o++)t[o]&&(r=p(t[o]))&&(a&&(a+=" "),a+=r)}else for(r in t)t[r]&&(a&&(a+=" "),a+=r);return a}function f(){for(var t,o,r=0,a="",s=arguments.length;r<s;r++)(t=arguments[r])&&(o=p(t))&&(a&&(a+=" "),a+=o);return a}const m=({children:t,bgImageUrl:o,onImageLoaded:r})=>{const[a,s]=l.useState(!1),c=()=>{s(!0),r&&r()};return e.jsxs(d,{className:f({show:a}),children:[e.jsx(n,{src:o,loading:"lazy",onLoad:c}),e.jsx(g,{children:t})]})};export{m as W,g as a,f as c};
