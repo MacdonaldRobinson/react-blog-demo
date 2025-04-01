022338653892d1fbf4a5c01c3100befc3c6d868f
Mac <maconly2001@gmail.com>
2025-04-01 09:47:14 -0600

    Updated blog page style

--- src/pages/BlogPage/BlogPage.styles.tsx
+++ src/pages/BlogPage/BlogPage.styles.tsx
@@ -1,14 +1,24 @@
 import styled from "styled-components";
 
-export const BlogPageWrapper = styled.div``;
+export const BlogPageWrapper = styled.div`
+    padding: 10px;
+`;
+export const BlogFilterWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 200px;
+    margin-bottom: 10px;
+`;
+
 export const BlogPagePostsWrapper = styled.div`
-  display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
-  align-items: center;
-  justify-content: center;
-  gap: 10px;
-  width: 100%;
-  height: 90vh;
-  overflow: auto;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: center;
+    gap: 10px;
+    width: 100%;
+    height: 85vh;
+    overflow: auto;
+    padding: 20px;
 `;
