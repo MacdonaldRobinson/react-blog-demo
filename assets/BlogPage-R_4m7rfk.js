c971c97b7c966f0af5dab7b96b229a4de0f9bc71
MacdonaldRobinson <MacdonaldRobinson@users.noreply.github.com>
2025-04-03 22:52:10 +0000

    deploy: 993ecc54a9310531523c1aecec2fc41f0967604b

--- assets/BlogPage-R_4m7rfk.js
+++ assets/BlogPage-R_4m7rfk.js
@@ -1,11 +1,11 @@
-import{d as o,r as p,j as t,R as w,B as v}from"./index-B6Smc-K9.js";import{c as j,a as b,W as y}from"./WrapWithBgImage-NviqoRAP.js";const P=o.div`
+import{d as o,R as w,r as d,j as t,B as v}from"./index-DbPLNrKi.js";import{c as j,a as b,W as y}from"./WrapWithBgImage-Bpo5By0J.js";const P=o.div`
     padding: 10px;
-`,B=o.div`
+`,C=o.div`
     display: flex;
     flex-direction: column;
     width: 200px;
     margin-bottom: 10px;
-`,C=o.div`
+`,B=o.div`
     display: flex;
     flex-direction: row;
     flex-wrap: wrap;
@@ -28,7 +28,7 @@ import{d as o,r as p,j as t,R as w,B as v}from"./index-B6Smc-K9.js";import{c as 
     filter: blur(5px);
 `,h=o.div``,u=o.div`
     transform: rotateY(180deg);
-`,c=o.div`
+`,p=o.div`
     opacity: 0;
 `,W=o.div`
     color: black;
@@ -51,7 +51,7 @@ import{d as o,r as p,j as t,R as w,B as v}from"./index-B6Smc-K9.js";import{c as 
             opacity: 1;
         }
 
-        ${c} {
+        ${p} {
             opacity: 1;
 
             & > div {
@@ -60,7 +60,7 @@ import{d as o,r as p,j as t,R as w,B as v}from"./index-B6Smc-K9.js";import{c as 
             }
         }
 
-        ${c} {
+        ${p} {
             & > div {
                 transition: all 1s;
                 animation: slideInDown 1s 0s forwards;
@@ -79,7 +79,7 @@ import{d as o,r as p,j as t,R as w,B as v}from"./index-B6Smc-K9.js";import{c as 
         }
     }
 
-    ${f}, ${u}, ${c}, ${h} {
+    ${f}, ${u}, ${p}, ${h} {
         position: absolute;
         top: 0;
         left: 0;
@@ -102,11 +102,11 @@ import{d as o,r as p,j as t,R as w,B as v}from"./index-B6Smc-K9.js";import{c as 
         }
     }
 
-    ${c} {
+    ${p} {
         text-align: center;
     }
 
-    ${c},
+    ${p},
     ${h} {
         padding: 10px;
     }
@@ -140,7 +140,7 @@ import{d as o,r as p,j as t,R as w,B as v}from"./index-B6Smc-K9.js";import{c as 
             transform: rotateY(360deg);
         }
     }
-`,k=({bgImageUrl:e,children:i,backFaceContent:r,frontFaceHeaderContent:a})=>{const[n,l]=p.useState(!1),[d,m]=p.useState(!1),x=()=>{m(!0)},s=()=>{r&&l(!n)};return t.jsxs(F,{className:j({show:d,flip:n}),onClick:s,children:[t.jsxs(f,{children:[!d&&t.jsx(W,{children:"Loading..."}),t.jsx(g,{src:e,onLoad:x,loading:"lazy"}),t.jsxs(c,{children:[a&&t.jsx($,{children:a}),t.jsx("div",{children:i})]})]}),r&&t.jsxs(u,{children:[t.jsx(g,{src:e,onLoad:x,loading:"lazy"}),t.jsx(h,{children:t.jsx("div",{children:r})})]})]})},L=w.memo(({fullText:e,highlightText:i})=>{if(!e)return e;const r=new RegExp(`(${i})`,"gi");return e.split(r).map((n,l)=>n.toLowerCase()==i.toLowerCase()?t.jsx("mark",{children:n},l):n)}),I=o.div`
+`,k=w.memo(({bgImageUrl:e,children:i,backFaceContent:r,frontFaceHeaderContent:a})=>{const[n,l]=d.useState(!1),[c,m]=d.useState(!1),x=()=>{m(!0)},s=()=>{r&&l(!n)};return t.jsxs(F,{className:j({show:c,flip:n}),onClick:s,children:[t.jsxs(f,{children:[!c&&t.jsx(W,{children:"Loading..."}),t.jsx(g,{src:e,onLoad:x,loading:"lazy"}),t.jsxs(p,{children:[a&&t.jsx($,{children:a}),t.jsx("div",{children:i})]})]}),r&&t.jsxs(u,{children:[t.jsx(g,{src:e,onLoad:x,loading:"lazy"}),t.jsx(h,{children:t.jsx("div",{children:r})})]})]})}),L=w.memo(({fullText:e,highlightText:i})=>{if(!e)return e;const r=new RegExp(`(${i})`,"gi");return e.split(r).map((n,l)=>n.toLowerCase()==i.toLowerCase()?t.jsx("mark",{children:n},l):n)}),I=o.div`
   display: flex;
   flex-direction: column;
   gap: 10px;
@@ -206,4 +206,4 @@ import{d as o,r as p,j as t,R as w,B as v}from"./index-B6Smc-K9.js";import{c as 
     background-color: rgba(0, 0, 0, 0.5);
     cursor: pointer;
     padding: 5px;
-`,R=({title:e,content:i,bgImageUrl:r,onPopupModelCloseCallback:a})=>{const[n,l]=p.useState(),d=()=>{l(!0)};return t.jsxs(z,{children:[!n&&t.jsx(t.Fragment,{children:"Loading..."}),t.jsx(E,{className:j({show:n}),children:t.jsxs(y,{bgImageUrl:r,onImageLoaded:d,children:[t.jsx(D,{onClick:a,children:"X Close"}),t.jsx(T,{children:e}),t.jsx(Y,{children:i})]})})]})},H=()=>{const e=p.useContext(v),[i,r]=p.useState(""),[a,n]=p.useState(null),l=e.blogPosts.filter(s=>!!s.body.toLowerCase().includes(i.toLowerCase())),d=s=>{r((s==null?void 0:s.currentTarget.value)??"")},m=s=>{n(s)},x=()=>{n(null)};return t.jsxs(P,{children:[t.jsxs(B,{children:[t.jsx("label",{htmlFor:"BlogFilter",children:"Filter Blog Posts"}),t.jsx("input",{id:"BlogFilter",value:i,onInput:d})]}),t.jsxs("fieldset",{children:[t.jsxs("label",{children:["Blog Posts: ",l.length]}),t.jsxs(C,{children:[e.isLoading&&t.jsx(t.Fragment,{children:"Loading ..."}),e.isError&&t.jsx(t.Fragment,{children:"Error Loading"}),!e.isLoading&&l.length==0&&t.jsx(t.Fragment,{children:"No blog posts found"}),l.map(s=>t.jsx(M,{blogPost:s,filterText:i,popupBlogPost:m},s.id))]})]}),a&&t.jsx(R,{bgImageUrl:`https://via.assets.so/album.png?id=${a.id}&q=95&w=360&h=360&fit=fill`,title:a.title,content:a.body,onPopupModelCloseCallback:x})]})};export{H as default};
+`,R=({title:e,content:i,bgImageUrl:r,onPopupModelCloseCallback:a})=>{const[n,l]=d.useState(),c=()=>{l(!0)};return t.jsxs(z,{children:[!n&&t.jsx(t.Fragment,{children:"Loading..."}),t.jsx(E,{className:j({show:n}),children:t.jsxs(y,{bgImageUrl:r,onImageLoaded:c,children:[t.jsx(D,{onClick:a,children:"X Close"}),t.jsx(T,{children:e}),t.jsx(Y,{children:i})]})})]})},H=()=>{const e=d.useContext(v),[i,r]=d.useState(""),[a,n]=d.useState(null),l=e.blogPosts.filter(s=>!!s.body.toLowerCase().includes(i.toLowerCase())),c=s=>{r((s==null?void 0:s.currentTarget.value)??"")},m=d.useCallback(s=>{n(s)},[]),x=()=>{n(null)};return t.jsxs(P,{children:[t.jsxs(C,{children:[t.jsx("label",{htmlFor:"BlogFilter",children:"Filter Blog Posts"}),t.jsx("input",{id:"BlogFilter",value:i,onInput:c})]}),t.jsxs("fieldset",{children:[t.jsxs("label",{children:["Blog Posts: ",l.length]}),t.jsxs(B,{children:[e.isLoading&&t.jsx(t.Fragment,{children:"Loading ..."}),e.isError&&t.jsx(t.Fragment,{children:"Error Loading"}),!e.isLoading&&l.length==0&&t.jsx(t.Fragment,{children:"No blog posts found"}),l.map(s=>t.jsx(M,{blogPost:s,filterText:i,popupBlogPost:m},s.id))]})]}),a&&t.jsx(R,{bgImageUrl:`https://via.assets.so/album.png?id=${a.id}&q=95&w=360&h=360&fit=fill`,title:a.title,content:a.body,onPopupModelCloseCallback:x})]})};export{H as default};
--- assets/HomePage-BQ8sdzeT.js
+++ assets/HomePage-BQ8sdzeT.js
@@ -1,4 +1,4 @@
-import{d as e,R as i,j as t,r as l,B as c}from"./index-B6Smc-K9.js";import{W as d}from"./WrapWithBgImage-NviqoRAP.js";const m=e.section`
+import{d as e,R as i,j as t,r as l,B as c}from"./index-DbPLNrKi.js";import{W as d}from"./WrapWithBgImage-Bpo5By0J.js";const m=e.section`
   height: 90vh;
 `,p=e.div`
   height: 100%;
