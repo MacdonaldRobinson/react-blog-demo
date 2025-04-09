462a352277ced5cdc6c9ff749c9a078385018351
Mac <maconly2001@gmail.com>
2025-04-08 18:46:51 -0600

    fixed issue with blog mobile

--- src/pages/BlogPage/BlogPageItem/BlogPageItem.styles.tsx
+++ src/pages/BlogPage/BlogPageItem/BlogPageItem.styles.tsx
@@ -1,19 +1,21 @@
 import styled from "styled-components";
 
 export const BlogPageItemWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-  width: 20vw;
-  height: 20vw;
-  position: relative;
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    width: 20vw;
+    height: 20vw;
+    min-width: 200px;
+    min-height: 200px;
+    position: relative;
 `;
 
 export const BlogPostPopupWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-end;
-  margin-right: 10px;
-  margin-top: 10px;
-  font-size: 0.8rem;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+    margin-right: 10px;
+    margin-top: 10px;
+    font-size: 0.8rem;
 `;
