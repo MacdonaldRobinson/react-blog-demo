764c5462a599884e79cf97912ac3c926d7635295
Mac <maconly2001@gmail.com>
2025-04-17 16:09:57 -0600

    Fixed issue with CMS Page height and scrollbar

--- src/pages/CMSPage/CMSPage.styles.tsx
+++ src/pages/CMSPage/CMSPage.styles.tsx
@@ -1,6 +1,8 @@
 import styled from "styled-components";
 
 export const CMSPageWrapper = styled.section`
+    overflow: auto;
+    height: 94vh;
     padding: 10px;
     display: flex;
     flex-direction: column;
